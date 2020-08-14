d1ef6198b2d96013d1fa8acfbf397c5c909fb73d
Juliana Villegas <jmvilleg@bancolombia.com.co>
2020-08-14 16:30:36 -0500

    html inicial header y voting boxes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import VotingBox from './components/VotingBox';
 
 function App() {
+  let celebrities = [
+    {name: "Kanye"},
+    {name: "Kim"},
+    {name: "Taylor"},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="/">Past trials</a></li>
+            <li><a href="/">How it works?</a></li>
+            <li><a href="/">Log-In / Sign-Up</a></li>
+          </ul>
+        </nav>
+        <form method="put" action="">
+          <fieldset>
+            <legend>What is your opinion on Pope Francis?</legend>
+            <input id="like" type="radio" name="opinion" value="Like"/>
+            <label className="like-dislike-icon" htmlFor="like">L</label>
+            <input id="dislike" type="radio" name="opinion" value="Dislike"/>
+            <label className="like-dislike-icon" htmlFor="dislike">D</label>
+            <input type="submit" value="Vote now"/>
+          </fieldset>
+        </form>
       </header>
+      <h2>Previous polls</h2>
+      <ul>
+      {celebrities.map((celebrity)=>{
+        return <VotingBox key={celebrity.name} name={celebrity.name}/>
+      })}
+      </ul>
     </div>
   );
 }
---
+++ src/components/VotingBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const VotingBox = (props) => {
+    return ( 
+    <li className="voting-box">
+        <form method="put" action="">
+          <fieldset>
+                <legend>What is your opinion on {props.name}</legend>
+            <input id="like" type="radio" name="opinion" value="Like"/>
+            <label htmlFor="like"></label>
+            <input id="dislike" type="radio" name="opinion" value="Dislike"/>
+            <label htmlFor="dislike"></label>
+            <input type="submit" value="Vote now"/>
+          </fieldset>
+        </form>
+    </li> );
+}
+ 
+export default VotingBox;
