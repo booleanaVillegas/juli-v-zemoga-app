3295e306e0b35b1a78e6eaf3cbe8ffd6372a1c4c
Juliana Villegas <jmvilleg@bancolombia.com.co>
2020-08-14 18:01:59 -0500

    spacing del layour principal

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import React from 'react';
 import './App.scss';
-import VotingBox from './components/VotingBox';
+import VotingBox from './components/VotingBox/VotingBox';
 
 function App() {
   let celebrities = [
-    {name: "Kanye"},
-    {name: "Kim"},
-    {name: "Taylor"},
+    { name: "Kanye" },
+    { name: "Kim" },
+    { name: "Taylor" },
+    { name: "Malala" },
   ]
 
   return (
     <div className="App">
-      <header>
-        <nav>
-          <ul>
-            <li><a href="/">Past trials</a></li>
-            <li><a href="/">How it works?</a></li>
-            <li><a href="/">Log-In / Sign-Up</a></li>
-          </ul>
-        </nav>
+      <div className="hero">
+        <header>
+          <span>
+            <a href="/">logo</a>
+          </span>
+          <nav className="nav-container">
+            <ul>
+              <li><a href="/">Past trials</a></li>
+              <li><a href="/">How it works?</a></li>
+              <li><a href="/">Log-In / Sign-Up</a></li>
+            </ul>
+          </nav>
+        </header>
         <form method="put" action="">
           <fieldset>
             <legend>What is your opinion on Pope Francis?</legend>
-            <input id="like" type="radio" name="opinion" value="Like"/>
+            <input id="like" type="radio" name="opinion" value="Like" />
             <label className="like-dislike-icon" htmlFor="like">L</label>
-            <input id="dislike" type="radio" name="opinion" value="Dislike"/>
+            <input id="dislike" type="radio" name="opinion" value="Dislike" />
             <label className="like-dislike-icon" htmlFor="dislike">D</label>
-            <input type="submit" value="Vote now"/>
+            <input type="submit" value="Vote now" />
           </fieldset>
         </form>
-      </header>
-      <h2>Previous polls</h2>
-      <ul>
-      {celebrities.map((celebrity)=>{
-        return <VotingBox key={celebrity.name} name={celebrity.name}/>
-      })}
-      </ul>
+      </div>
+      <main>
+        <section>
+          <h2>Votes</h2>
+          <ul>
+            {celebrities.map((celebrity) => {
+              return <VotingBox key={celebrity.name} name={celebrity.name} />
+            })}
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/VotingBox/VotingBox.js
+++ src/components/VotingBox/VotingBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './VotingBox.scss'
 
 const VotingBox = (props) => {
     return ( 
