e4ba4411169fd04c573c296843b261d3e38976bc
Juliana Villegas <jmvilleg@bancolombia.com.co>
2020-08-16 22:23:18 -0500

    updated Readme

--- src/containers/MainVoting/MainVoting.test.js
+++ src/containers/MainVoting/MainVoting.test.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, queryByLabelText } from '@testing-library/react';
 import "@testing-library/jest-dom";
 import MainVoting from './MainVoting';
-/*
+
 describe("MainVoting component unit testing", ()=>{
 
-    it("should render the pope's name",()=>{
-        render(<MainVoting/>);
-        const { getByText } = render(<MainVoting character={{name: "Pope"}} />);
-        const loading = getByText("Pope");
-        expect(loading).toBeInTheDocument();
+    const propsCelebrity ={
+        name: "John Doe",
+        image: "/",
+        description: "This is a description",
+        likes: 10,
+        dislikes: 23,
+        dateAdded: {}
+    }
 
+    it("should render the name sent in props",()=>{
+        // Arrange
+        // Act
+        const renderResult = render(<MainVoting celebrity={propsCelebrity} />);
 
+        let nameElem = renderResult.container.querySelector("h1");
 
-
-
+        // Assert
+        expect(nameElem.textContent).toBe(propsCelebrity.name+"?");
+    
     });
 }
-); */
+); 
