734a2aa4d8b8bc38a376140d829c1a0adaa03ad1
Juliana Villegas <jmvilleg@bancolombia.com.co>
2020-08-17 14:38:16 -0500

    pruebas unit the componente voting box

--- src/components/VotingBox/VotingBox.test.js
+++ src/components/VotingBox/VotingBox.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, fireEvent, screen} from '@testing-library/react';
 import "@testing-library/jest-dom";
+
 import VotingBox from './VotingBox';
 
-describe("VotingBox unit tests", ()=>{
 
-    const propsCelebrity ={
+describe("VotingBox unit tests", () => {
+
+    const propsCelebrity = {
         name: "John Doe",
         image: "/",
         description: "This is a description",
@@ -13,10 +15,13 @@ describe("VotingBox unit tests", ()=>{
         dislikes: 23,
         dateAdded: {}
     }
-    it("should render the name sent in props",()=>{
+
+    const mockFn = (param1,param2)=>{console.log("yeeehaaaah!")};
+
+    it("should render the name sent in props", () => {
         // Arrange
+        const renderResult = render(<VotingBox character={propsCelebrity}/>);
         // Act
-        const renderResult = render(<VotingBox character={propsCelebrity} />);
 
         let nameElem = renderResult.container.querySelector("h1");
 
@@ -24,18 +29,50 @@ describe("VotingBox unit tests", ()=>{
         expect(nameElem.textContent).toBe(propsCelebrity.name);
 
     });
-    it("enable the 'vote now' btn when i click the like btn",()=>{
+    it("enable the 'vote now' btn when i click the like btn", () => {
         // Arrange
+        const renderResult = render(<VotingBox character={propsCelebrity}/>);
         // Act
-        const renderResult = render(<VotingBox character={propsCelebrity} />);
-
         let voteNow = renderResult.container.querySelector("button");
         let like = renderResult.container.querySelector(".like");
 
-        like.dispatchEvent(new Event("click"));
+        fireEvent.click(like);
 
         // Assert
         expect(voteNow.disabled).toBe(false);
+
+    });
+    it("change text btn to 'vote again' when the like or dislike is sent", () => {
+        // Arrange
+
+        const renderResult = render(<VotingBox character={propsCelebrity} updateVotes={(id,obj)=>mockFn(id,obj)}/>);
+        // Act
+        let voteNow = renderResult.container.querySelector("button");
+        let like = renderResult.container.querySelector(".like");
+
+        fireEvent.click(like);
+        fireEvent.click(voteNow);
+
+        // Assert
+
+        expect(voteNow.textContent).toBe("Vote Again?");
+
+    });
+    it("change the like/dislike buttons back to visible when i click voteAgain", () => {
+        // Arrange
+
+        const renderResult = render(<VotingBox character={propsCelebrity} updateVotes={(id,obj)=>mockFn(id,obj)}/>);
+        // Act
+        let voteNow = renderResult.container.querySelector("button");
+        let like = renderResult.container.querySelector(".like");
+
+        fireEvent.click(like);
+        fireEvent.click(voteNow);
+        fireEvent.click(voteNow);
+
+        // Assert
+
+        expect(like.style.display).toBe("flex");
 
     });
 })
